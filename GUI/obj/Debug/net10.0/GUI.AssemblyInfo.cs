aff5a798cffdf14913cf0fb857b733d9e10987dd
Rocky <alexwaldmann2004@gmail.com>
2026-04-10 23:48:23 -0600

    Bricks mostly fixed, good enough for me

--- GUI/obj/Debug/net10.0/GUI.AssemblyInfo.cs
+++ GUI/obj/Debug/net10.0/GUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f35d3c5cc19b8c09f12178e0ae8324ce3fc1b607")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84081ed3da22065984272b51e58afb31eef9fe1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
